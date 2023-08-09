b53e36e53719e07e4941024a1ac3cdd47df41bd2
nurcholisalfachri <nurcholis.alfachri@gmail.com>
2023-08-09 22:52:09 +0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,12 @@
-import Image from "next/image";
 
-export async function getDogData() {
-  const res = await fetch ("https://dog.ceo/api/breeds/image/random", {next: {revalidate: 3}})
-  return res.json();
-}
-
-
-
-const Home = async () => {
-
-  const [dog] = await Promise.all<any>([
-    getDogData()
-  ])
-
+const Home = () => {
+  
   return (
     <div className="flex font-bold">
       <div className="mx-auto my-10">
         <div className="flex">
           <div className="mx-auto my-5">HOME PAGE</div>
         </div>
-        <Image src={dog.message} alt="dog" width={300} height={300}/>
-        <p className="my-3">Cute Dog ❤</p>
       </div>
     </div>
   )
