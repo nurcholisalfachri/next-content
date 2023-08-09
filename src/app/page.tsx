03d2884247a91f46123251059fdb63370f3fd47f
nurcholisalfachri <nurcholis.alfachri@gmail.com>
2023-08-09 23:08:58 +0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+import Image from "next/image";
 
 const Home = () => {
   
   return (
     <div className="flex font-bold">
       <div className="mx-auto my-10">
-        <div className="flex">
+        <div className="flex my-10">
           <div className="mx-auto my-5">HOME PAGE</div>
         </div>
+        <Image src="/vercel.svg" width={200} height={200} alt="profile" />
       </div>
     </div>
   )
